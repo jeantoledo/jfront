87676f9f1be017cd23ab13cba599494c800a9d7a
Jean Toledo <jeanctoledo@gmail.com>
2017-10-02 08:02:22 -0300

    Format webpack config

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -2,23 +2,25 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 
+const webpackPlugins = [
+  new CleanWebpackPlugin(['dist']),
+  new HtmlWebpackPlugin({
+    template: 'app/index.html',
+    inject: true
+  })
+];
+
+const webpackModuleRules = [
+  { test: /\.html$/, exclude: /node_modules/, use: 'html-loader' },
+  { test: /\.js$/, exclude: /node_modules/, use: "babel-loader" }
+];
+
 module.exports = {
   entry: './app/app.js',
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: 'bundle.js'
   },
-  plugins: [
-    new CleanWebpackPlugin(['dist']),
-    new HtmlWebpackPlugin({
-      template: 'app/index.html',
-      inject: true
-    })
-  ],
-  module: {
-    rules: [
-      { test: /\.html$/, exclude: /node_modules/, use: 'html-loader' },
-      { test: /\.js$/, exclude: /node_modules/, use: "babel-loader" }
-    ]
-  }
+  plugins: webpackPlugins,
+  module: { rules: webpackModuleRules }
 }
