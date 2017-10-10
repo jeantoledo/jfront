c44ff3c859e61022d77e03f258319acddc65481a
Jean Toledo <jeanctoledo@gmail.com>
2017-10-10 07:28:18 -0300

    Simulate a build error

--- app/components/JSection/index.jsx
+++ app/components/JSection/index.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const JSection = props => (
-  <section className="section">
-    <div className="container is-fluid">
-      <h1 className="title">{props.title}</h1>
-      <h2 className="subtitle">{props.subtitle}</h2>
+  <section className="section2">
+    <div className="container2 is-fluid">
+      <h1 className="title2">{props.title}</h1>
+      <h2 className="subtitle2">{props.subtitle}</h2>
     </div>
   </section>
 );
