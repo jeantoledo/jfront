ce25b42e7297fe9917111269dd4f96e2b5b9580c
Jean Toledo <jeanctoledo@gmail.com>
2017-12-12 23:27:27 -0200

    Fix ESLint rule

--- app/components/JSection/index.test.jsx
+++ app/components/JSection/index.test.jsx
@@ -3,9 +3,6 @@ import renderer from 'react-test-renderer';
 import JSection from './index';
 
 test('JSection render title and subtitle props', () => {
-  const component = renderer.create(
-    <JSection title="JFront" subtitle="Javascript Single Page Application" />,
-  );
-
-  expect(component).toMatchSnapshot();
+  const component = <JSection title="JFront" subtitle="Javascript Single Page Application" />;
+  expect(renderer.create(component)).toMatchSnapshot();
 });
