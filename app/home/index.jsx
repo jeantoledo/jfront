4974b9e04cd5d289091a01d14ce610fc777543ce
Jean Toledo <jeanctoledo@gmail.com>
2018-03-12 07:35:08 -0300

    setup aws

--- app/home/index.jsx
+++ app/home/index.jsx
@@ -3,7 +3,7 @@ import JSection from './../components/JSection/jsection';
 import jbones from '../assets/images/jbones.png';
 
 const Home = () => (
-  <JSection title="JFront" subtitle="Javascript Single Page Application" logo={jbones} />
+  <JSection title="JFront" subtitle="Javascript Single Page Application (aws)" logo={jbones} />
 );
 
 export default Home;
